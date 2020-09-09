33c7b599d59f2240e6f8594ed682384ace6bc00b
Enrique Hilst <enrique.hilst@gmail.com>
2020-09-09 12:05:11 -0500

    startup config

---
+++ AsyncApp/Data/SchoolDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncApp.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- AsyncApp/Startup.cs
+++ AsyncApp/Startup.cs
@@ -2,20 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AsyncApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AsyncApp.Data;
 
 namespace AsyncApp
 {
     public class Startup
     {
+        // 1. 
+        public IConfiguration Configuration { get; }
+
+        // 2. 
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // 3. Register DbContext with the app
+            services.AddDbContext<SchoolDbContext>(options =>
+            {
+                // DATABASE_URL
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
