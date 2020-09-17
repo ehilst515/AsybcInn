011fd0762d267b4cb36a7902e1c632124c7249b6
Enrique Hilst <enrique.hilst@gmail.com>
2020-09-16 23:01:46 -0500

    Registration errors handle improvements

--- AsyncApp/Services/IUserService.cs
+++ AsyncApp/Services/IUserService.cs
@@ -1,9 +1,9 @@
 ﻿using AsyncApp.Models;
 using AsyncApp.Models.API;
+using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace AsyncApp.Services
 {
@@ -14,9 +14,26 @@ namespace AsyncApp.Services
 
     class IdentityUserService : IUserService
     {
-        public Task<ApplicationUser> Register(RegisterData data)
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public IdentityUserService(UserManager<ApplicationUser> userManager)
         {
-            throw new NotImplementedException();
+            this.userManager = userManager;
+        }
+        public async Task<ApplicationUser> Register(RegisterData data)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = data.Username,
+                Email = data.Email,
+                PhoneNumber = data.PhoneNumber,
+
+            };
+
+            await userManager.CreateAsync(user, data.Password);
+
+            return user;
+
         }
     }
 }
