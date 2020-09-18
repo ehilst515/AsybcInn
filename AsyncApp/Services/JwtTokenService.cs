ee8b7b0660912bf132af6d5a0840b0eabc0a80d7
Enrique Hilst <enrique.hilst@gmail.com>
2020-09-18 00:08:16 -0500

    Add JwtTokenService logic

--- AsyncApp/Services/JwtTokenService.cs
+++ AsyncApp/Services/JwtTokenService.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AsyncApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace AsyncApp.Services
 {
     public class JwtTokenService
     {
+        private IConfiguration configuration;
+        private SignInManager<ApplicationUser> signInManager;
+
+        public JwtTokenService(IConfiguration configuration, SignInManager<ApplicationUser> signInManager)
+        {
+            this.configuration = configuration;
+            this.signInManager = signInManager;
+
+        }
     }
 }
